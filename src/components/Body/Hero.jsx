d0539a7347f5cc718b69e15b5097a5a1a774185e
simon409 <addarm409@gmail.com>
2023-11-12 10:29:42 +0100

    fixed hero animation

--- src/components/Body/Hero.jsx
+++ src/components/Body/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import gsap from "gsap";
 import { useOutroContext } from "../../Provider/OutroProvider";
 import { useTranslation } from "react-i18next";
